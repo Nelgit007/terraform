5af329db9755d6c8ed608ea832a3cccd69f5b9df
Nelgit007 <osagienelson24@gmail.com>
2022-09-06 10:58:11 +0100

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-provider "aws" {
-  region  = "us-east-1"
-  access_key = "AK***************WD"
-  secret_key = "tYQr***OF*****************u"
-}
-
+# Create Ec2
 resource "aws_instance" "My-server" {
   ami           = "ami-0729e439b6769d6ab"
   instance_type = "t2.micro"
